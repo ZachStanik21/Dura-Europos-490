ebeb4dd44b3c0d4f17d7072c7608a09e129ff8d0
ZachStanik21 <zachary.stanik@yale.edu>
2021-03-25 21:36:32 -0400

    Create Vue application and update Django settings

--- duraproject/duraproject/urls.py
+++ duraproject/duraproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from duraapp import views as duraapp_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vue-test', duraapp_views.vue_test),
 ]
