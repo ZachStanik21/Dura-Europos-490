cf24d3bbb59abf798fcf01faf57c34e8d59fa7bc
ZachStanik21 <zachary.stanik@yale.edu>
2021-04-21 21:40:06 -0400

    Heroku app deployment

--- duraproject/duraproject/wsgi.py
+++ duraproject/duraproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duraproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duraproject.duraproject.settings')
 
 application = get_wsgi_application()
