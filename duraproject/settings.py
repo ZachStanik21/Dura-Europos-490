5505f89d5202ea5a91327660c982eb3082522908
ZachStanik21 <zachary.stanik@yale.edu>
2021-05-12 20:25:06 -0400

    490 development build and security fixes

--- duraproject/settings.py
+++ duraproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*s7+8x$(*=eo161msgdtajg-8sox6rqp8e#b+^195hv=27o3!f'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
